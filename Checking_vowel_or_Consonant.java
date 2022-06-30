df708e05cce7b9ffbf5cddbc72b9c7cdc3d81c40
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-06-30 18:12:48 +0530

    Add files via upload

---
+++ Checking_vowel_or_Consonant.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Checking_vowel_or_Consonant
+{
+    public static void main(String[] args)
+{
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter alphabet: ");
+        char ch= sc.next().charAt(0);
+        if((ch=='a' || ch== 'e' || ch== 'i' || ch=='o' || ch=='u') || 
+            (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'))
+        {
+           System.out.println("vowel");
+        }
+        else
+        {
+            System.out.println("consonant");
+        }
+    }
+}
