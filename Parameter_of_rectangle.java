b8bd312b7f12515ef8b122902f39ce8b477830c6
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-06-30 17:53:27 +0530

    Add files via upload

---
+++ Parameter_of_rectangle.java
@@ -0,0 +1,8 @@
+public class Parameter_of_rectangle {
+  public static void main(String[] args)
+  {
+    int l=10,w=6,P;
+    P=l+w;
+    System.out.println(" Parameter_of_rectangle = "+(2*P));
+  }
+}
