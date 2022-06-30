274552997db3675bb10accc791d4af290c9512d0
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-06-30 18:05:39 +0530

    Add files via upload

--- Area_of_triangle.java
+++ Area_of_triangle.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class Area_of_triangle {
   public static void main(String[] args)
   {
-    int h=10,b=20;
+    Scanner sc=new Scanner(System.in);
+    int h=sc.nextInt();
+    int b=sc.nextInt();
     System.out.println(" Area_of_triangle = "+(h*b)*0.5);
   }
 }
