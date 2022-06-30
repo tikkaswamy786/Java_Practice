eea8a1c38fe4072012ce729524a54b459ccc756e
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-06-30 17:55:12 +0530

    Add files via upload

---
+++ Sum_of_square_of_two_numbers.java
@@ -0,0 +1,8 @@
+public class Sum_of_square_of_two_numbers {
+  public static void main(String[] args)
+  {
+    int g=20,h=60,Sum_of_square_of_two_numbers;
+    Sum_of_square_of_two_numbers = ((g*g)+(h*h));
+    System.out.println("Sum_of_square_of_two_numbers = "+Sum_of_square_of_two_numbers);
+  }
+}
