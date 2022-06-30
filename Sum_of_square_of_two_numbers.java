536170673d9af43e3cd0fe2d3efe31e250ccb2fa
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-06-30 18:03:24 +0530

    Add files via upload

--- Sum_of_square_of_two_numbers.java
+++ Sum_of_square_of_two_numbers.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class Sum_of_square_of_two_numbers {
   public static void main(String[] args)
   {
-    int g=20,h=60,Sum_of_square_of_two_numbers;
-    Sum_of_square_of_two_numbers = ((g*g)+(h*h));
+    Scanner sc=new Scanner(System.in);
+    int g=sc.nextInt();
+    int h=sc.nextInt();
+    
+    int Sum_of_square_of_two_numbers = ((g*g)+(h*h));
     System.out.println("Sum_of_square_of_two_numbers = "+Sum_of_square_of_two_numbers);
   }
 }
