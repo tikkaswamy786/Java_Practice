47c7abc31098a94ed9741d0027f971160745e2c8
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-06-30 18:04:57 +0530

    Add files via upload

--- Multiplication.java
+++ Multiplication.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 public class Multiplication {
   public static void main(String[] args)
   {
-    int a=10,b=20;
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
     System.out.println("a*b = "+(a*b));
   }
 }
