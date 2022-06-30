f351bb1151ea4b21cc2ad68d9149e97c1ecf7af5
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-06-30 18:06:04 +0530

    Add files via upload

--- Area_of_square.java
+++ Area_of_square.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 public class Area_of_square {
   public static void main(String[] args)
   {
-    int a=10;
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
     System.out.println("Area_of_square = "+(a*a));
   }
 }
