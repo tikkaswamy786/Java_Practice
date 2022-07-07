22e4aacf3bd12b9ee865f48f5cb08460e34f2df0
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-07-07 18:15:29 +0530

    Add files via upload

---
+++ numberReverseOrder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class numberReverseOrder   
+{  
+  public static void main(String[] args)   
+  {  
+    int number,reverse = 0;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    number= sc.nextInt();
+    while(number != 0)   
+    {  
+      int remainder = number % 10;  
+      reverse = reverse * 10 + remainder;  
+      number = number/10;  
+    }  
+    System.out.println("The reverse of the given number is: " + reverse);  }  
+}
