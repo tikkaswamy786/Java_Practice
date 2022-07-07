71ab550aa5f005114fdf01e358fb62fdc4df6d84
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-07-07 18:14:27 +0530

    Add files via upload

---
+++ Strong_number.java
@@ -0,0 +1,35 @@
+public class Main
+ {
+   public static void main (String[]args)
+   {
+     int num = 145;
+     if (detectStrong (num))
+         System.out.println (num + " is Strong Number");
+     else
+         System.out.println (num + " is not a Strong Number");
+   }
+   static int facto (int n)
+   {
+     int fact = 1;
+     for (int i = 1; i <= n; i++)
+       fact = fact * i;
+     return fact;
+   }
+   static boolean detectStrong (int num)
+   {
+     int digit, sum = 0;
+     int temp = num;
+     boolean flag = false;
+     while (temp != 0)
+       {
+     	digit = temp % 10;
+     	sum = sum + facto (digit);
+ 	    temp /= 10;
+       }    
+     if (sum == num)
+       flag = true;
+     else
+       flag = false;
+     return flag;
+   }
+ }
