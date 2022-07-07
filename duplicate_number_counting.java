1a970e8dc870ab6a09a5c1bba67e87b466cf6220
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-07-07 18:35:45 +0530

    Add files via upload

---
+++ duplicate_number_counting.java
@@ -0,0 +1,19 @@
+public class duplicate_number_counting 
+{  
+    public static void main(String[] args) 
+  {       
+        int count=0;
+        int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};   
+        System.out.println("Duplicate elements in given array: ");    
+        for(int i = 0; i < arr.length; i++) 
+        {  
+            for(int j = i + 1; j < arr.length; j++) 
+            {  
+                if(arr[i] == arr[j])  
+                    count++; 
+            }  
+        }  
+        System.out.print("Duplicate number counting Number: "+count);
+    
+    }  
+}  
