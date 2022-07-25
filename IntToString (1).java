4fa8c39f98734037a0b87323ff4ff68c595b9056
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-07-25 12:16:11 +0530

    Add files via upload

---
+++ IntToString (1).java
@@ -0,0 +1,35 @@
+/*  JFM1T8_Assignment1:
+
+    Write a program to convert an int variable to String using an inbuilt function of String class and print the char at index position 2.
+    Prompt the user input from the terminal.
+    
+    Sample input: 54321
+    
+    Expected output: char at index position 2 = 3
+*/
+
+import java.util.Scanner;
+public class IntToString 
+{
+    public static void main(String args[])
+    {
+        int i;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a numbers : ");
+        i=sc.nextInt();
+        String s=Integer.toString(i);
+        char firstcharacter=s.charAt(2);
+        System.out.println("char at index position 2 = "+(firstcharacter));
+    }
+}
+//main method
+
+//declare int type variable
+
+//take input from user
+
+//converting integer to string using toString method
+
+//use inbuilt function to print index position
+
+//print result
