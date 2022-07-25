c5a34c5709f4b30e1ab486cb25fe8473c8f97a27
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-07-25 17:08:24 +0530

    Add files via upload

---
+++ Sport.java
@@ -0,0 +1,36 @@
+/* JFM1T9_Assignment5:
+	Create an object named obj of the class Main and print the reference to the object using this keyword.
+        
+        Sample Output:
+        Sport name: Football
+*/ 
+import java.util.Scanner;
+class  Sport 
+{ 
+// Create a member variable for Sport name 
+private String Sportname;
+public Sport(String Sportname)
+  {
+    this.Sportname=Sportname;
+  }
+
+// Set member variable for Sport name 
+public void setSportname(String newvalue)
+  {
+    Sportname=newvalue;
+  }
+public String getSportname()
+  {
+    return Sportname;
+  }
+public static void main(String args[]) 
+  { 
+Scanner sc=new Scanner(System.in);
+System.out.println("Sport name");
+String str=sc.nextLine();
+// Create object for Sport class 
+Sport obj=new Sport(str);
+System.out.println("Sport name: "+obj.getSportname());
+// Print the value of Sport name using "this" reference
+   } 
+}
