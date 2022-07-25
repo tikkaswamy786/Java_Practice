dc708feeb26327f7fc75d71d718d38b575ca9aa7
tikkaswamy786 <105532227+tikkaswamy786@users.noreply.github.com>
2022-07-25 12:10:14 +0530

    Add files via upload

---
+++ HighestNumber.java
@@ -0,0 +1,62 @@
+/* JFM1T4_Assignment1:
+   Write a program to find the highest among the given 3 numbers.
+   Prompt the user for the 3 numbers to be input from the terminal.
+
+   SampleInput: 
+   Enter the first number: 11
+   Enter the second number: 22
+   Enter the third number: 1
+
+   Expected Output:
+   22 is highest number
+ 	
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.*;
+public class HighestNumber
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three integer: ");
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        if(num1>num2 && num1>num3)
+        {
+            System.out.println(num1+" is highest number");
+        }
+        else if(num2>num1 && num2>num3)
+        {
+            System.out.println(num2+" is highest number");
+        }
+        else if(num3>num1 && num3>num2)
+        {
+            System.out.println(num3+" is highest number");
+        }
+        else
+        {
+            System.out.println("The numbers are same.");
+        }
+    }
+}
+     
+//Add main method
+
+//Declare the three variables
+
+//Use the scanner class to provide input at execution time
+/* 
+ Scanner s= new Scanner(System.in);
+ System.out.println("Enter the first number");
+ number1=s.nextInt();
+ 
+*/
+
+//check which number is highest using if else condition
+
+//Print the highest of three numbers
+ 
+//Good Job!
+//Please maintain appropriate comments in your code.
---
+++ Pattern.java
@@ -0,0 +1,46 @@
+/* JFM1T4_Assignment4:
+   Write a program to print the following pattern:
+     1 
+     01 
+     101 
+     0101 
+     10101 
+*/
+import java.util.Scanner;
+public class Pattern 
+{
+    public static void main(String[] args) 
+  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                if ((i + j) % 2 == 0) 
+                {
+                    System.out.print("1 ");
+                } 
+                else 
+                {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+     
+//Add main method here
+
+//Declare variable n and initialize value as 5
+
+//Check whether the i and j are equal or not using for loop
+
+//If i and j are equal check (i+j)%2==0 or not if zero then print one else print zero
+
+
+
+//Good Job!
+//Please maintain appropriate comments in your code.
